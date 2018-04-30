85fb296a427d2f2a8777118deac6d38fe5057cb9
Your Name <2314779927@qq.com>
2018-04-30 15:04:48 +0000

    Topic index page

---
+++ resources/views/topics/_sidebar.blade.php
@@ -0,0 +1,13 @@
+{{-- 右边导航栏 表示注释--}}
+
+<div class="panel panel-default">
+
+    <div class="panel-heading">
+    </div>
+
+    <div class="panel-body">
+    {{--<li class="active">右边导航栏</li>--}}
+    右边导航栏
+    </div>
+
+</div>

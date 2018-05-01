833fe8fe3fa15dfd08694fa98977e56f167c5c7d
Your Name <2314779927@qq.com>
2018-05-01 11:26:19 +0000

    Category topic list

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -20,6 +20,13 @@
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
+            <li class="{{ active_class(if_route('topics.index')) }}"><a href="{{ route('topics.index') }}">话题</a></li>
+                <li class="{{ active_class((if_route('categories.show') && if_route_param('category', 1))) }}"><a href="{{ route('categories.show', 1) }}">分享</a></li>
+                <li class="{{ active_class((if_route('categories.show') && if_route_param('category', 2))) }}"><a href="{{ route('categories.show', 2) }}">教程</a></li>
+                <li class="{{ active_class((if_route('categories.show') && if_route_param('category', 3))) }}"><a href="{{ route('categories.show', 3) }}">问答</a></li>
+                <li class="{{ active_class((if_route('categories.show') && if_route_param('category', 4))) }}"><a href="{{ route('categories.show', 4) }}">公告</a></li>
+            </ul>
+            <!--<ul class="nav navbar-nav">
              <li class="dropdown">
              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
 					致妈妈
@@ -33,10 +40,23 @@
              </li>
 
              <li><a href="#">我的故事</a></li>
+             <li class="dropdown">
+             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+					社区指导与讨论
+					<b class="caret"></b>
+			 </a>
+              <ul class="dropdown-menu">
+               <li class="active"><a href="{{ route('topics.index') }}">话题</a></li>
+                <li><a href="{{ route('categories.show', 1) }}">分享</a></li>
+                <li><a href="{{ route('categories.show', 2) }}">教程</a></li>
+                <li><a href="{{ route('categories.show', 3) }}">问答</a></li>
+                <li><a href="{{ route('categories.show', 4) }}">公告</a></li>
+              </ul>
+             </li>
              <li><a href="#">社区指导与讨论</a></li>
              <li><a href="#">教育与宣传</a></li>
 
-            </ul>
+            </ul>-->
 
             <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right">

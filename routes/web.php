2ff1ead12b3631fd26287d0889712e0a87696b2a
Your Name <2314779927@qq.com>
2018-05-09 14:17:10 +0000

    Auto translate slug

--- routes/web.php
+++ routes/web.php
@@ -35,10 +35,12 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 //对个人中心的增，删，改添加路由
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 //根据分类列表话题
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 //上传图片
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');

e716322238242f5f391cd23467f336e7c7f1c9ac
Your Name <2314779927@qq.com>
2018-05-10 14:15:45 +0000

    Notify author when topic replied

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -11,8 +12,11 @@ class ReplyObserver
 {
     public function creating(Reply $reply)
     {
+        $topic = $reply->topic;
         $reply->topic->increment('reply_count',1);
         $reply->content = clean($reply->content, 'user_topic_body');
+        // 通知作者话题被回复了
+        $topic->user->notify(new TopicReplied($reply));
     }
 
     public function updating(Reply $reply)
--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,6 @@ Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload
 
 Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
+
+//将消息通知显示出来
+Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);

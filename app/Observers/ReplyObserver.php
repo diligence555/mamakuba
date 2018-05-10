c01799f58df22107d57efff074b019e131305191
Your Name <2314779927@qq.com>
2018-05-10 14:40:11 +0000

    User can delete reply

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -13,10 +13,16 @@ class ReplyObserver
     public function creating(Reply $reply)
     {
         $topic = $reply->topic;
-        $reply->topic->increment('reply_count',1);
+        $reply->topic->increment('reply_count',1);//添加评论后使得计数器的数字加1
         $reply->content = clean($reply->content, 'user_topic_body');
         // 通知作者话题被回复了
         $topic->user->notify(new TopicReplied($reply));
+    }
+
+    //用于在删除评论后使得计数器的数字减1
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->decrement('reply_count', 1);
     }
 
     public function updating(Reply $reply)
